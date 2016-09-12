17fc3d9f0858e1ce336dcefb513c2a96afd5275b
Can Kattw <kattwinkel@w11k.de>
2016-09-12 10:17:46 +0200

    add the animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,26 @@ import {Component, trigger, state, style, transition, animate} from '@angular/co
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0, 0, 0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 export class AppComponent {
   title = 'app works!';
 
-  toggleMenu() {
+  menuState:string = 'out';
 
+  toggleMenu() {
+    // 1-line if statement that toggles the value:
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
   }
 }
